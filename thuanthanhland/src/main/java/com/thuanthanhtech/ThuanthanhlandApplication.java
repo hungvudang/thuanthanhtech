c5e2b005b181cd5f4fe0edf0e8f374ebc0b50bf8
hungvudang <hungvudang.bn@gmail.com>
2021-01-21 15:59:21 +0700

    initial commit

---
+++ thuanthanhland/src/main/java/com/thuanthanhtech/ThuanthanhlandApplication.java
@@ -0,0 +1,13 @@
+package com.thuanthanhtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThuanthanhlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThuanthanhlandApplication.class, args);
+	}
+
+}
---
+++ thuanthanhland/src/test/java/com/thuanthanhtech/ThuanthanhlandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thuanthanhtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThuanthanhlandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
