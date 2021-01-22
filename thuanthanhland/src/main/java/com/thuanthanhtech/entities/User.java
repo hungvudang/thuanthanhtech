a330dad680c7716d3d1670b5498a68d3c6f70aa1
hungvudang <hungvudang.bn@gmail.com>
2021-01-22 10:07:23 +0700

    [add] thymeleaf

---
+++ thuanthanhland/src/main/java/com/thuanthanhtech/entities/User.java
@@ -0,0 +1,138 @@
+package com.thuanthanhtech.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String name;
+	private String email;
+	private Integer role;
+	private String address;
+	private String password;
+	private String phone;
+	private String avatar;
+	private LocalDateTime created_at;
+	private LocalDateTime updated_at;
+	
+	public User() {
+		super();
+	}
+
+	public User(String name, String email, Integer role, String address, String password, String phone, String avatar,
+			LocalDateTime created_at, LocalDateTime updated_at) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.role = role;
+		this.address = address;
+		this.password = password;
+		this.phone = phone;
+		this.avatar = avatar;
+		this.created_at = created_at;
+		this.updated_at = updated_at;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getRole() {
+		return role;
+	}
+
+	public void setRole(Integer role) {
+		this.role = role;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public LocalDateTime getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(LocalDateTime created_at) {
+		this.created_at = created_at;
+	}
+
+	public LocalDateTime getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(LocalDateTime updated_at) {
+		this.updated_at = updated_at;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		this.created_at = LocalDateTime.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		this.updated_at = LocalDateTime.now();
+	}
+	
+	
+	
+}
