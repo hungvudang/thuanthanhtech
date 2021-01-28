9f276e3c8796571bee95b058b48125735bb5e6f7
hungvudang <hungvudang.bn@gmail.com>
2021-01-28 16:09:36 +0700

    [add] tạo entity Contact

--- thuanthanhland/src/main/java/com/thuanthanhtech/entities/User.java
+++ thuanthanhland/src/main/java/com/thuanthanhtech/entities/User.java
@@ -9,8 +9,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
