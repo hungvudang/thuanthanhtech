0de908613f49f476853f4b6e973e203e5c63dd97
hungvudang <hungvudang.bn@gmail.com>
2021-01-30 18:36:52 +0700

    [fix] update resources folder

--- thuanthanhland/src/main/java/com/thuanthanhtech/controllers/SlideAdminController.java
+++ thuanthanhland/src/main/java/com/thuanthanhtech/controllers/SlideAdminController.java
@@ -1,16 +1,13 @@
 package com.thuanthanhtech.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.thuanthanhtech.repositories.SlideRepository;
 
 @Controller
 @RequestMapping("/slide")
 public class SlideAdminController {
 	
-	@Autowired
-	private SlideRepository sRepository;
+//	@Autowired
+//	private SlideRepository sRepository;
 
 }
