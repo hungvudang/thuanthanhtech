29b2b0af6c3601cd0b243ce28dd52d1cb810be2d
hungv <hungv@thuanthanhland>
2021-03-04 14:45:55 +0700

    [update] giao diện người dùng

--- thuanthanhland/src/main/java/com/thuanthanhtech/controllers/client/ProjectClientController.java
+++ thuanthanhland/src/main/java/com/thuanthanhtech/controllers/client/ProjectClientController.java
@@ -3,6 +3,9 @@ package com.thuanthanhtech.controllers.client;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +13,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.thuanthanhtech.entities.Project;
-import com.thuanthanhtech.repositories.ProjectRepository;
+import com.thuanthanhtech.repositories.ProjectPagingAndSortRepository;
 
 @Controller
 @RequestMapping("/du-an")
 public class ProjectClientController {
 	
 	@Autowired
-	private ProjectRepository pRepository;
+	private ProjectPagingAndSortRepository pRepository;
 	
 	@GetMapping
 	public String project(Model m) {
-		List<Project> projects = pRepository.findByPub(1);
+		return "redirect:/du-an/page/1";
+	}
+	
+	@GetMapping("/page/{index}")
+	public String nextPage(@PathVariable("index") Integer index, Model m) {
+		Pageable pageable = PageRequest.of(index - 1, 8);
+		Page<Project> page = pRepository.findByPub(1, pageable);
+		
+		List<Project> projects = page.getContent();
+		int currentPage = index;
+		int totalPages = page.getTotalPages();
+		int numOfElements = page.getNumberOfElements();
 		
 		m.addAttribute("projects", projects);
+		m.addAttribute("currentPage", currentPage);
+		m.addAttribute("totalPages", totalPages);
+		m.addAttribute("numOfElements", numOfElements);
+		
 		return "public-pages/project";
 	}
 	
