34cd49865a44cc94567f7c1caf18be2f130b2c1a
hungvudang <hungvudang.bn@gmail.com>
2021-03-23 13:14:24 +0700

    [refactor] code

--- thuanthanhland/src/main/java/com/thuanthanhtech/controllers/client/RegisterClientController.java
+++ thuanthanhland/src/main/java/com/thuanthanhtech/controllers/client/RegisterClientController.java
@@ -1,13 +1,10 @@
 package com.thuanthanhtech.controllers.client;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,8 +13,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.thuanthanhtech.entities.Category;
-import com.thuanthanhtech.entities.Helper;
 import com.thuanthanhtech.entities.User;
 import com.thuanthanhtech.repositories.CategoryRepository;
 import com.thuanthanhtech.repositories.UserRepository;
