0f9e93b3a1a983c4021f3984ceaad63a95fe2fe0
hungvudang <hungvudang.bn@gmail.com>
2021-03-27 20:45:01 +0700

    [feat] thêm tiếng việt cho datatable và summernote

--- thuanthanhland/src/main/java/com/thuanthanhtech/ThuanthanhlandApplication.java
+++ thuanthanhland/src/main/java/com/thuanthanhtech/ThuanthanhlandApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,16 +17,17 @@ import com.thuanthanhtech.repositories.UserRepository;
 @SpringBootApplication
 public class ThuanthanhlandApplication extends SpringBootServletInitializer  implements CommandLineRunner {
 
-	
+	private static String ADMIN_EMAIL = "admin@thuanthanh.com";
+	private static String ADMIN_PASSWORD = "admin@admin";
 	@Autowired
 	private UserRepository uRepository;
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 	
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(ThuanthanhlandApplication.class);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ThuanthanhlandApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ThuanthanhlandApplication.class, args);
@@ -33,7 +35,7 @@ public class ThuanthanhlandApplication extends SpringBootServletInitializer  imp
 
 	@Override
 	public void run(String... args) throws Exception {
-		User admin = new User(null, "Adminstrator", "admin@thuanthanh.com", 1, "Ha Noi", passwordEncoder.encode("admin@admin"), "0918273645", Helper.NO_IMAGE_MEDIUM_PNG, null, null);
+		User admin = new User(null, "Adminstrator", ADMIN_EMAIL , 1, "Ha Noi", passwordEncoder.encode(ADMIN_PASSWORD), "0918273645", Helper.NO_IMAGE_MEDIUM_PNG, null, null);
 		Optional<User> opAdmin = uRepository.findByEmail(admin.getEmail());
 		if (!opAdmin.isPresent()) {
 			uRepository.save(admin);
